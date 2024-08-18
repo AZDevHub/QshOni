c3dd4c5e0bb196288fa3e862a897af2ad28521d9
Alex Zaw <alex.zaw@azdevhub.com>
2024-08-18 15:47:10 -0400

    Rename CLPs to CLLE using rename_clp_to_clle.sh script to be able to use these as SQL Procedures

---
+++ rename_clp_to_clle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Function to recursively rename .clp files to .clle
+rename_clp_to_clle() {
+    local dir="$1"
+    
+    # Find all files in the directory matching *.clp (case insensitive)
+    find "$dir" -type f -iname "*.clp" | while read -r file
+    do
+        # Get the base name without the extension
+        base_name="${file%.*}"
+        
+        # Build the new filename with .clle extension
+        new_file="${base_name}.clle"
+        
+        # Rename the file
+        mv "$file" "$new_file"
+        
+        # Log the change
+        echo "Renamed: $file to $new_file"
+    done
+}
+
+# Specify the directory you want to start the search from
+directory_to_search="$1"
+
+# Call the function to rename .clp files to .clle
+rename_clp_to_clle "$directory_to_search"
